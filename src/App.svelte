dfe38f41cb454944ce39e4e9f9ff74b8fc54b0a7
kingstonnadar <nadarkingston096@gmail.com>
2024-07-07 22:29:36 +0530

    Update App.svelte
    code on svelte

--- src/App.svelte
+++ src/App.svelte
@@ -6,48 +6,50 @@
   let title = '';
   let note = '';
 
- onMount(() => {
-  const savedPages = localStorage.getItem("pages");
-  if (savedPages) {
-    pages = JSON.parse(savedPages);
-    title = pages[currentPageIndex]  || "New Page";
-    note = localStorage.getItem(title);
-  } else {
-    addPage();
+  onMount(() => {
+    const savedPages = localStorage.getItem("pages");
+    if (savedPages) {
+      pages = JSON.parse(savedPages);
+      title = pages[currentPageIndex];
+      note = localStorage.getItem(title);
+    } else {
+      addPage();
+    }
+  });
+
+  function saveNote() {
+    const storedPageName = pages[currentPageIndex];
+    if (storedPageName != title) {
+      localStorage.removeItem(storedPageName);
+      pages[currentPageIndex] = title;
+    }
+    localStorage.setItem(title, note);
+    localStorage.setItem("pages", JSON.stringify(pages));
   }
- 
- });
 
- function saveNote() {
-   pages[currentPageIndex] = title;
-   localStorage.setItem('title',note);
-   localStorage.setItem('pages', JSON.stringify(pages));
+  function addPage() {
+    pages.push("New Page");
+    selectPage(pages.length ? pages.length - 1 : 0);
+  }
 
- } 
-
-function addPage() {
-  pages.push("New Page");
-  selectPage(pages.length ? pages.length - 1 : 0)
-}
-
-function selectPage(index) {
-  currentPageIndex = index;
-  title = pages[currentPageIndex];
-  note = localStorage.getItem(title);
-}
+  function selectPage(index) {
+    currentPageIndex = index;
+    title = pages[currentPageIndex];
+    note = localStorage.getItem(title);
+  }
 </script>
 
 <aside class="fixed top-0 left-0 z-40 w-60 h-screen">
-  <div class="bg-light-gray overflow-y-auto py-5 px-3 h-full border-r border-gray-200">
-    <ul class="space-y-2"> 
-      {#each pages as page, index}
-      <li>        
-        <button on:click={()=>selectPage(index)} class="bg-dark-gray py-2 px-3 text-gray-900 rounded-lg">{title || "New Page"}</button>
+<div class="bg-light-gray overflow-y-auto py-5 px-3 h-full border-r border-gray-200">
+  <ul class="space-y-2">
+    {#each pages as page, index}
+    <li>
+        <button on:click={()=>selectPage(index)} class="{index == currentPageIndex ? 'bg-dark-gray' : ''} py-2 px-3 text-gray-900 rounded-lg">{page}</button>
     </li>
     {/each}
     <li class="text-center"><button on:click={addPage} class="font-medium">+ Add page</button></li>
-    </ul>
-  </div>
+  </ul>
+</div>
 </aside>
 
 <main class="p-4 ml-60 h-auto">
@@ -56,7 +58,7 @@ function selectPage(index) {
     <button class="ml-auto bg-gray-800 text-white px-5 py-2.5 rounded-lg font-medium text-sm mt-3 hover:bg-gray-900" on:click={saveNote}>Save</button>
   </div>
   <hr/>
-<textarea class="mt-3 block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={note}></textarea>
+  <textarea class="mt-3 block w-full bg-gray-50 border border-gray-300 rounded-lg text-gray-900 p-2.5" bind:value={note}></textarea>
 </main>
 
 <style>
@@ -65,6 +67,6 @@ function selectPage(index) {
 }
 
 .bg-dark-gray {
-  background:  #EFEFEF;
+  background: #EFEFEF;
 }
 </style>
